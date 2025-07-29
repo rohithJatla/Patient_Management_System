7bf2d0b3d2198de0ce29217f2e5928f02b43f1a4
Rohith Jatla <jatlar1@udayton.edu>
2025-07-29 17:44:49 -0400

    forgot to add these to previous commit

---
+++ patient_service/src/main/java/com/rohith/jatla/patient_service/dto/PatientRequestDTO.java
@@ -0,0 +1,68 @@
+package com.rohith.jatla.patient_service.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class PatientRequestDTO {
+
+    @NotBlank
+    @Size(max = 100,message = "Name cannot exceed 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Not a valid Email")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "DOB required")
+    private  String dateOfBirth;
+
+    @NotBlank(message = "Registered Date required")
+    private String registeredDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
+
+}
---
+++ patient_service/src/main/java/com/rohith/jatla/patient_service/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.rohith.jatla.patient_service.exceptions;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler{
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String,String> errors = new HashMap<>();
+        ex.getBindingResult()
+                .getFieldErrors()
+                        .forEach(error ->
+                                errors.put(
+                                        error.getField(),
+                                        error.getDefaultMessage()
+                                ));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
